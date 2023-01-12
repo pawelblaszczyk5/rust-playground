2f4b797e435fe27822d837e0f9e6f96b35dc4b88
pawelblaszczyk5 <pawelblaszczyk@wir.pl>
2023-01-12 17:08:40 +0100

    Guessing game

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,32 @@
+use rand::Rng;
+use std::{cmp::Ordering, io};
+
 fn main() {
-    println!("Hello, world!");
+    println!("Guess the number!");
+
+    let secret_number = rand::thread_rng().gen_range(1..=100);
+
+    loop {
+        let mut guess = String::new();
+        println!("Please input your guess.");
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Error while retriving user input");
+
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+
+        println!("You guessed: {guess}");
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("The number is too small"),
+            Ordering::Equal => {
+                println!("You guessed correctly");
+                break;
+            }
+            Ordering::Greater => println!("The number is too big"),
+        }
+    }
 }
